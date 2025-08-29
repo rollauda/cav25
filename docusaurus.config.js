a0fac10d46d6629468e37243e461f4a5f9800770
rollauda <rolland.auda@gmail.com>
2025-08-29 11:47:47 -0500

    up

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/cinema.svg',
 
   // Set the production url of your site here
-  url: 'https://profauda.fr',
+  url: 'https://cav1.profauda.fr',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/cav25/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
